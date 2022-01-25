394250897d09858269aa568da08ccdfec91c73c0
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2022-01-25 19:39:17 +0100

    Update info.py

--- info.py
+++ info.py
@@ -52,6 +52,7 @@ IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE","<b>Query: {query}</b> \n‌‌‌�
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "True"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+COMPRESSOR = is_enabled((environ.get('COMPRESSOR', "True")), True)
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.")
