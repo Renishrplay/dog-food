575c3a4a157441c694c5e6f54071f4a7daede405
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2022-01-27 00:04:27 +0100

    Update corona.py

--- plugins/admemes/corona.py
+++ plugins/admemes/corona.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 API = "https://api.sumanjay.cf/covid/?country="
 
-BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Join our Channel ⚙', url='https://t.me/team_annaben')]])
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ ⭕️Join MOVIE 🍿 CHANNEL ⭕️ ⚙', url='https://t.me/rplaymovie')]])
 
 @Client.on_message(filters.command("covid"))
 async def reply_info(client, message):
