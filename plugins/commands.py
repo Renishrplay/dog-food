871a33c51ddc93aeb8ed3e1d03289778934fa8e6
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2022-01-26 19:36:52 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -8,7 +8,7 @@ from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details
 from database.users_chats_db import db
-from info import CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, LOG_CHANNEL, PICS
+from info import CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, PROTECT_CONTENT
 from utils import get_size, is_subscribed, temp
 
 logger = logging.getLogger(__name__)
