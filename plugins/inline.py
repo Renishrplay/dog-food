5fc0ae37eff8041c78bbe2d9e4f4a1e6d7ae1c71
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2022-01-29 19:22:56 +0100

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -19,6 +19,21 @@ async def inline_users(query: InlineQuery):
 @Client.on_inline_query()
 async def answer(bot, query):
     """Show search results for given inline query"""
+@Client.on_inline_query()
+async def inline(bot, query):
+          searche = query.query
+          if searche.startswith("1"):
+                await inlineX1(bot, query)
+          elif searche.startswith("2"):
+                await inlineX2(bot, query)
+          elif searche.startswith("3"):
+                await inlineX3(bot, query)
+          elif searche.startswith("4"):
+                await inlineX4(bot, query)
+          else:
+               await results00(bot, query)
+async def inlineX1(bot, update):
+          answers = []
     
     if not await inline_users(query):
         await query.answer(results=[],
