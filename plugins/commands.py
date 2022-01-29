126a2a23076b6db27eb8e5b585bbdb0161c463bd
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2022-01-29 15:15:56 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,11 +23,6 @@ STICKERS = (
 
 @Client.on_message(filters.command("start") & filters.incoming & ~filters.edited)
 async def start(client, message):
-    await message.reply_sticker(random.choice(STICKERS))
-    await get_start_func(message)
-
-@Client.on_message(filters.command("start") & filters.incoming & ~filters.edited)
-async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
@@ -60,7 +55,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
-            photo=random.choice(PICS),
+            photo=random.choice(PICS) + STICKERS,
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
@@ -103,7 +98,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
-            photo=random.choice(PICS),
+            photo=random.choice(PICS) + STICKERS,
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
