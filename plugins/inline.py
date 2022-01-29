1c868c9b24487b789194f4185a95358b2270cb9d
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2022-01-29 23:44:18 +0100

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -85,6 +85,10 @@ async def answer(bot, query):
             pass
         except Exception as e:
             logging.exception(str(e))
+            await query.answer(results=[], is_personal=True,
+                           cache_time=cache_time,
+                           switch_pm_text=str(e)[:63],
+                           switch_pm_parameter="error")
     else:
         switch_pm_text = f'{emoji.CROSS_MARK} No results'
         if string:
